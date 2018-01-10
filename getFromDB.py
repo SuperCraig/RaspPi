0a232fa9a67daec4c74dd23d6ca3b2376c26b066
SuperCraig <spadeb47339@gmail.com>
2018-01-10 22:34:32 +0800

    Add files via upload

---
+++ getFromDB.py
@@ -0,0 +1,31 @@
+#import a database connection
+import MySQLdb
+import sys
+
+#open a database connection
+#be sure to change the host IP address, username, password and database name
+# to match your own
+connection = MySQLdb.connect("52.199.2.57" , "mbiuser" , "mbi16948543" , "street_light_para" )
+
+#prepare a cursor object using cursor() method
+cursor = connection.cursor()
+
+#execute the SQL query using execute() method
+cursor.execute("select * from street_light_para.light_config")
+
+#fetch all of the rows from the query
+data = cursor.fetchall()
+
+#print the rows
+for row in data:
+	print row[0], row[1], row[2], row[3]
+
+#close the cursor object
+cursor.close()
+
+#close the connection
+cursor.close()
+
+#exit the program
+sys.exit()
+
